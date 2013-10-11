c066c2451a22052a7c5658526c1934589c8c49e0
Eduardo Díaz <iamedu@gmail.com>
2013-10-11 14:06:47 -0500

    Adding port

--- introspect-server/src/clj/introspect/server/core.clj
+++ introspect-server/src/clj/introspect/server/core.clj
@@ -2,17 +2,18 @@
   (:use org.httpkit.server
         compojure.core)
   (:require [clojure.tools.logging :as log]
-            [introspect.server.util :as util]
-            [clojure.tools.cli :refer (cli)]))
+            [clojure.tools.cli :refer (cli)]
+            [compojure.route :as route]
+            [introspect.server.util :as util]))
 
-(defn app [req]
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    "hello HTTP!"})
+(defroutes app
+  (GET "/"  [] "<h1>Hello World</h1>")
+  (route/not-found "<h1>Page not found</h1>"))
 
 (defn -main [& args]
   (let [[options args banner] (cli args
                                    ["-p" "--port" "Port to listen on" :default 3000 :parse-fn #(Integer/parseInt %)]
                                    ["-d" "--database" "Database url" :parse-fn util/parse-db])
         {:keys [port database]} options]
-    (log/info "Starting server on port and database" port (:db database))))
+    (log/info "Starting server on port" port "database" (:db database))
+    (run-server app {:port port})))
